86ad79531a212a0e1e5188abab13a0bd008adb78
Faizan <2k23.cs2314006@gmail.com>
2026-01-22 13:35:32 +0530

    built register page and also connected backend to store user

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-
+import { Toaster } from 'react-hot-toast'
+import RegisterPage from './components/RegisterPage'
 function App() {
 
   return (
     <BrowserRouter>
       <Navbar />
+      <Toaster position="bottom-center" richColors />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                             </Link>
                         </li>
                     }
-                    {!
+                    {
                         <Link to="/register">
                             <li className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
                                 SignUp
@@ -67,7 +67,7 @@ const Navbar = () => {
                         </Link>
                     }
 
-                    {
+                    {!
                         <button
                             // onClick={onLogOutHandler}
                             className="sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
