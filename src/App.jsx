e77d15f62414a6c8f47158382c3c31ce4756c3ac
Faizan <2k23.cs2314006@gmail.com>
2026-02-07 17:20:39 +0530

    fixed URL redirection problem occur after deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { BrowserRouter } from 'react-router-dom';
-import { getApps } from './utils/helper'
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ShortenUrlPage from "./components/ShortenUrlPage";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import AboutPage from "./components/AboutPage";
+import RegisterPage from "./components/RegisterPage";
+import Login from "./components/Login";
+import DashboardLayout from "./components/dashboard/DashboardLayout";
+import PrivateRoute from "./PrivateRoute";
+import ErrorPage from "./components/ErrorPage";
 function App() {
-  const CurrentApp = getApps();
   return (
     <BrowserRouter>
-      <CurrentApp />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/register" element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
+        <Route path="/login" element={<PrivateRoute publicPage={true}><Login /></PrivateRoute>} />
+        <Route path="/dashboard" element={<PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/:url" element={<ShortenUrlPage />} />
+      </Routes>
+      <Footer />
+      <Toaster />
     </BrowserRouter>
   )
 }
--- src/components/ShortenUrlPage.jsx
+++ src/components/ShortenUrlPage.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Loader from './Loader';
 
 const ShortenUrlPage = () => {
     const { url } = useParams();
 
     useEffect(() => {
         if (url) {
-            window.location.href = import.meta.env.VITE_BACKEND_URL + `/${url}`;
+            window.location.href = import.meta.env.VITE_BACKEND_URL + `${url}`;
         }
     }, [url]);
-    return <p>Redirecting...</p>;
+    return <>
+        <p className='text-center text-[80px] font-bold text-slate-800'>Redirecting to Your URL...</p>
+        <Loader />
+    </>;
 }
 
 export default ShortenUrlPage
